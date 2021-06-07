adc37a6fd4433be23d4074e0f1471a3da4d6ede0
yuzujoe <yuzun225@gmail.com>
2021-06-08 08:20:34 +0900

    add docker

--- self-host-runner/userdata.sh
+++ self-host-runner/userdata.sh
@@ -2,6 +2,19 @@
 
 # Install
 yum -y install jq git
+yum -y install libcurl libcurl-devel ruby-devel gcc-c++
+
+amazon-linux-extras install docker -y
+service docker start
+usermod -a -G docker ec2-user
+
+curl -L https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+curl -sL https://rpm.nodesource.com/setup_14.x | bash -
+yum -y install nodejs
 
 su ec2-user -c 'mkdir $HOME/actions-runner'
 su ec2-user -c 'curl -L https://github.com/actions/runner/releases/download/v2.277.1/actions-runner-linux-x64-2.277.1.tar.gz -o $HOME/actions-runner/actions-runner-linux-x64-2.277.1.tar.gz'
